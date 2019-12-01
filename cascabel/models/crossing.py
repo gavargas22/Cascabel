035ab740f57401a6592de5c6d4ce9a173ddfd367
Guillermo Vargas <gavargas22@gmail.com>
2019-11-30 20:57:00 -0600

    Continuing work

---
+++ cascabel/models/crossing.py
@@ -0,0 +1,11 @@
+class Crossing():
+    '''
+    Crossing
+    ========
+
+    The final reported data for how long it took for a person
+    to cross the bridge
+    '''
+
+    def __init__(self):
+        pass
